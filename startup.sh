b2bd7328a9c7a8097c47a4a0aa91e24646a51246
Aslidd1nKhan <81762797+Aslidd1nKhan@users.noreply.github.com>
2024-11-22 16:50:39 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/AsmSafone/MusicPlayer /MusicPlayer
+git clone https://github.com/Aslidd1nKhan/Music- /MusicPlayer
 
 echo ">> INSTALLING REQUIREMENTS..."
 cd /MusicPlayer
